de1bf5b17f9a454e5cab305c4985cb7c7be5dea8
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-27 23:03:37 -0500

    a

--- src/main/java/org/example/logic/controller/HotelConsultService.java
+++ src/main/java/org/example/logic/controller/HotelConsultService.java
@@ -1,26 +1,42 @@
 package org.example.logic.controller;
-
+import org.example.views.SearchReservation;
 import org.example.logic.dao.DaoHuesped;
+import org.example.logic.dao.DaoReservation;
 import org.example.logic.entitites.Huesped;
+import org.example.logic.entitites.Reserve;
 import org.example.logic.utils.JPAUtils;
+import org.example.views.SearchReservation;
 
 import javax.persistence.EntityManager;
+import javax.swing.table.DefaultTableModel;
 import java.util.List;
 
 public class HotelConsultService {
 
 
-    public HotelConsultService() {
-    }
 
-    public void consultGuest(){
+
+    private void cargarDatosTablaReserva() {
+
+        DefaultTableModel modelo = (DefaultTableModel) SearchReservation.tbReservas.getModel();
+        modelo.setRowCount(0);
 
         EntityManager em = JPAUtils.getEntityManager();
-        DaoHuesped daoHuesped = new DaoHuesped(em);
-        List<Huesped> huespedList = daoHuesped.consultHuesped();
-        huespedList.forEach(huesped -> System.out.println(huesped.getName()));
+        DaoReservation reservaDao = new DaoReservation(em);
+        List<Reserve> reserves = reservaDao.getAllReservas();
 
-
+        for (Reserve reserve : reserves) {
+            Object[] fila = new Object[]{
+                    reserve.getId(),
+                    reserve.getCheckIn(),
+                    reserve.getCheckOut(),
+                    reserve.getBookingValue(),
+                    reserve.getPaymentMethod()
+            };
+            modelo.addRow(fila);
+        }
     }
+
+
 
 }
